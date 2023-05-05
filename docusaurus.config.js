19fc6d9b905fb2c27f55138b46d5d0d1d3484607
Senol <senols@gmail.com>
2023-05-05 13:24:40 +0300

    ok

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://sensahin.github.io',
+  url: 'https://aipowerorg.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'sensahin', // Usually your GitHub org/user name.
-  projectName: 'sensahin.github.io', // Usually your repo name.
+  organizationName: 'aipowerorg', // Usually your GitHub org/user name.
+  projectName: 'aipowerorg.github.io', // Usually your repo name.
   trailingSlash: false,
   deploymentBranch: 'deployment',
 
