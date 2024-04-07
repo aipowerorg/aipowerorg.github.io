7c1040a3a95728fc70915def3b88a24bc18cc485
Senol <senols@gmail.com>
2024-04-07 13:05:07 +0300

    new showcase

--- src/pages/showcase/index.js
+++ src/pages/showcase/index.js
@@ -5,39 +5,46 @@ const FLAG_BASE_URL = 'https://cdn.jsdelivr.net/gh/hjnilsson/country-flags@maste
 
 const showcases = [
     {
-        customerName: 'Carolyn',
+        customerName: 'Wordtap',
         flagUrl: `${FLAG_BASE_URL}/fr.svg`,
         description: `Carolyn's site provides a unique tool for language learning. Users input vocabulary, and the tool crafts a personalized story for active learning and enhanced retention.`,
         link: 'https://wordtap.net/create-a-story-using-these-words/',
         thumbnail: 'wordtap.png',
     },
     {
-        customerName: 'Tony',
+        customerName: 'Faith Guide',
         flagUrl: `${FLAG_BASE_URL}/us.svg`,
         description: `FaithGuide is an AI-powered app providing quick and reliable answers about Catholic teachings. The app also allows users to share their experiences, creating a supportive community.`,
         link: 'https://majellan.media/faithguide/',
         thumbnail: 'majellan.media.png',
     },
     {
-        customerName: 'Josh',
+        customerName: 'Job Hunt',
         flagUrl: `${FLAG_BASE_URL}/us.svg`,
         description: `Josh's website offers Instant Career Services, all built with AI Forms, including Resumes, Cover Letters, Career Paths, Salary Negotiation and Job Descriptions.`,
         link: 'https://jobhuntmode.com',
         thumbnail: 'jobhuntmode.png',
     },
     {
-        customerName: 'Anthony',
+        customerName: 'Law Smart',
         flagUrl: `${FLAG_BASE_URL}/us.svg`,
         description: `Introducing Alyssa! Alyssa is a Law Smart chatbot specifically trained on Florida state statutes as it pertains to family law issues here in the greater Tampa Bay area.`,
         link: 'https://taylordangelonelaw.com',
         thumbnail: 'alyssa.png',
     },
     {
-        customerName: 'Robert',
+        customerName: 'Documend',
         flagUrl: `${FLAG_BASE_URL}/us.svg`,
         description: `Documend helps nurses document faster and more accurate; audits chart before submission and notifies healthcare workers of missed assessments or interventions.`,
         link: 'https://documend.io/webapp/',
         thumbnail: 'documend.png',
+    },
+    {
+        customerName: 'Human Design',
+        flagUrl: `${FLAG_BASE_URL}/us.svg`,
+        description: `A chatbot that adds astrology, quantum physics, the chakra system, and other ancient systems into a tool which allows the users to understand who they are or on an energetic and quantum level. `,
+        link: 'https://humandesign.ai/',
+        thumbnail: 'humandesign.png',
     },
 ];
 
